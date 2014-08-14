6a298dc5d202ef69329f97e92c0e41adaa93332f
CrazyFreeMan <yaroslav.romanenko@gmail.com>
2014-08-14 12:58:16 +0300

    додано українську мову

---
+++ prjsender/lang/prjsender.ua.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['prjsender'] = 'Підписка на проекти';
+$L['prjsender_title'] = 'Налаштування підписки';
+$L['prjsender_desc'] = 'Будь ласка, відзначте категорії проектів для підписки:';
+
+$L['prjsender_mail_title'] = 'Нові проекти';
